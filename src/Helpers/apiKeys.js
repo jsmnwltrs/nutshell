0cfae2941be92f94efe339894ad97312ffcd09da
Jasmine Walters <jsmnwltrs@gmail.com>
2019-01-07 21:08:30 -0600

    setup project and auth

---
+++ src/Helpers/apiKeys.js
@@ -0,0 +1,12 @@
+const apiKeys = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyD_uf5sAUM5csv4SiAkDReA14zsNdsQSws',
+    authDomain: 'nutshell-9b96f.firebaseapp.com',
+    databaseURL: 'https://nutshell-9b96f.firebaseio.com',
+    projectId: 'nutshell-9b96f',
+    storageBucket: 'nutshell-9b96f.appspot.com',
+    messagingSenderId: '644742587121',
+  },
+};
+
+export default apiKeys;
