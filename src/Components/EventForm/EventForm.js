4fa92c4d0a8e5ea109f67420327556f8b1295ac8
Jasmine Walters <jsmnwltrs@gmail.com>
2019-01-13 17:21:48 -0600

    add works

--- src/Components/EventForm/EventForm.js
+++ src/Components/EventForm/EventForm.js
@@ -8,6 +8,7 @@ import {
 } from 'reactstrap';
 import './EventForm.scss';
 import PropTypes from 'prop-types';
+import authRequests from '../../Helpers/data/authRequests';
 
 const defaultEvent = {
   uid: '',
@@ -32,12 +33,24 @@ class EventForm extends React.Component {
     this.setState({ newEvent: tempEvent });
   }
 
+  formFieldNumberState = (name, e) => {
+    e.preventDefault();
+    const tempEvent = { ...this.state.newEvent };
+    tempEvent[name] = e.target.value;
+    this.setState({ newEvent: tempEvent });
+  }
+
   nameChange = e => this.formFieldStringState('event', e)
+
+  locationChange = e => this.formFieldStringState('location', e)
+
+  dateChange = e => this.formFieldNumberState('startDate', e)
 
   formSubmit = (e) => {
     e.preventDefault();
     const { onSubmit } = this.props;
     const myEvent = { ...this.state.newEvent };
+    myEvent.uid = authRequests.getCurrentUid();
     onSubmit(myEvent);
     this.setState({ newEvent: defaultEvent });
   }
@@ -45,7 +58,7 @@ class EventForm extends React.Component {
   render() {
     const { newEvent } = this.state;
     return (
-      <Form>
+      <Form onSubmit={this.formSubmit}>
         <FormGroup>
           <Label for="exampleName">Event Name:</Label>
           <Input
@@ -59,11 +72,25 @@ class EventForm extends React.Component {
         </FormGroup>
         <FormGroup>
           <Label for="exampleDate">Event Date:</Label>
-          <Input type="date" name="date" id="exampleDate" placeholder="type event date" />
+          <Input
+            type="date"
+            name="date"
+            id="exampleDate"
+            placeholder="type event date"
+            value={newEvent.startDate}
+            onChange={this.dateChange}
+          />
         </FormGroup>
         <FormGroup>
           <Label for="exampleLocation">Event Location:</Label>
-          <Input type="location" name="location" id="exampleLocation" placeholder="type event location" />
+          <Input
+            type="location"
+            name="location"
+            id="exampleLocation"
+            placeholder="type event location"
+            value={newEvent.location}
+            onChange={this.locationChange}
+          />
         </FormGroup>
         <Button>Submit</Button>
       </Form>
--- src/Helpers/props/eventShape.js
+++ src/Helpers/props/eventShape.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 
-const eventShape = {
+const eventShape = PropTypes.shape({
   uid: PropTypes.string,
   event: PropTypes.string,
   startDate: PropTypes.number,
   location: PropTypes.string,
-};
+});
 
 export default eventShape;
